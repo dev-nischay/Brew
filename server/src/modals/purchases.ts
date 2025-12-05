1d1525aa25d981f3228accb9c0478c57471f9e1f
dev-nischay <nischay.mehn@gmail.com>
2025-12-06 00:43:27 +0530

    further schema's added

---
+++ server/src/modals/purchases.ts
@@ -0,0 +1,36 @@
+import mongoose, { Types } from "mongoose";
+
+const purchaseSchema = new mongoose.Schema({
+  userId: {
+    type: Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+
+  currency: {
+    type: String,
+    default: "INR",
+    required: true,
+  },
+
+  purchasedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  totalAmount: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+
+  items: [
+    {
+      productId: {
+        types: Types.ObjectId,
+        required: true,
+        ref: "product",
+      },
+    },
+  ],
+});
