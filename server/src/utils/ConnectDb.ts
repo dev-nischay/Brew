4cbc0765021b38c022e71e8cab976c9ca73a93df
dev-nischay <nischay.mehn@gmail.com>
2025-12-06 00:45:37 +0530

    added database connection

---
+++ server/src/utils/ConnectDb.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const mongoUrl = process.env.MONGO_URL;
+export const connectDb = async () => {
+  try {
+    if (mongoUrl) {
+      await mongoose.connect(mongoUrl as string);
+      console.log("Db connected successfully");
+    } else {
+      console.error("Db connection failed");
+    }
+  } catch (error) {
+    console.error(
+      `Something went wrong while connecting database Error: ${error} `
+    );
+  }
+};
