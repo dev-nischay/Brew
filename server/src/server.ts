f7fa4b8da5073a4d4f5bb6223f4e3e3b3079e04c
dev-nischay <nischay.mehn@gmail.com>
2025-12-09 00:27:56 +0530

    updated core file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,28 @@
+import "dotenv/config";
 import express from "express";
 const app = express();
+app.use(express.json());
+import { adminRouter } from "./routes/adminRoutes/adminAuth.js";
+import { userRouter } from "./routes/userRoutes/userAuth.js";
+import { error } from "./middlewares/error.js";
 import type { Request, Response, NextFunction } from "express";
+import AppError from "./utils/AppError.js";
+import { connectDb } from "./utils/connectDb.js";
+import { logger } from "./middlewares/logger.js";
+import { HttpStatus } from "./types/enums.js";
+const Port = process.env.PORT;
 
-app.post("/", (req: Request, res: Response, next: NextFunction) => {
-  try {
-  } catch (error) {}
+app.use(logger);
+app.use("/api/v1/admin/", adminRouter);
+app.use("/api/v1/user/", userRouter);
+
+app.all(/.*/, (req: Request, res: Response, next: NextFunction) => {
+  return next(new AppError("Invalid Route", HttpStatus.NotFound));
 });
 
-app.listen(3000, () => {
-  console.log("App running at 3000");
+app.use(error);
+
+app.listen(Port, async () => {
+  await connectDb();
+  console.log(`Server running at Port ${Port}`);
 });
