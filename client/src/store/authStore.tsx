10db7cc33cc9ef2fcdd0c8ae8ec4f2f3dda47815
dev-nischay <nischay.mehn@gmail.com>
2025-12-11 00:32:14 +0530

    added zustand store for global state management

---
+++ client/src/store/authStore.tsx
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+type AuthStore = {
+  token: string;
+  username: string;
+  setToken?: (token: string) => void;
+  setUsername?: (username: string) => void;
+};
+
+export const useAuthStore = create(
+  persist<AuthStore>(
+    (set) => ({
+      token: "",
+      username: "",
+      setToken: (token) => {
+        set({ token });
+        localStorage.setItem("Authorization", `Bearer ${token}`);
+      },
+      setUsername: (username: string) => set(() => ({ username })),
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({ token: state.token, username: state.username }),
+      onRehydrateStorage: (state) => {
+        if (state?.token) {
+          localStorage.setItem("Authorization", `Bearer ${state.token}`);
+        }
+      },
+    }
+  )
+);
