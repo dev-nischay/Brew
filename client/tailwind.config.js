cabd39c1892bcdd0bf74214f6f9aef5fa135a5db
dev-nischay <nischay.mehn@gmail.com>
2025-12-14 02:25:44 +0530

    updated config file with custom styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#634832",
+        foreground: "#f7f7f7",
+        coffee: {
+          light: "#ece0d1",
+          dark: "#38220f",
+        },
+      },
+
+      fontFamily: {
+        wenkai: [
+          "'LXGW WenKai Mono TC'",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "monospace",
+        ],
+      },
+    },
   },
   plugins: [],
 };
